be651e049a49cb467887b530d4d504caf5afc53f
Camellia <88993589+camellia-1H@users.noreply.github.com>
2023-03-17 21:08:05 +0700

    fix merge

--- js/script.js
+++ js/script.js
@@ -15,3 +15,16 @@ function hideMobileNav(e) {
     $('.mobile-menu_btn').show();
     $('.navbar').removeClass('active');
 }
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '6b44941691mshf4c5c8b63ac4464p12ac97jsnd757cb99e014',
+        'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com',
+    },
+};
+
+fetch('https://moviesdatabase.p.rapidapi.com/titles/x/titles-by-ids?idsList=tt0001702%2Ctt0001856%2Ctt0001856', options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
